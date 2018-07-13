fa0aa9c698e33b8a7ff1db2ba1cd65183106f4d2
Ivan <ivan248959@gmail.com>
2018-07-14 00:34:57 +0200

    PDF export for KIF finished

--- project/src/main/java/com/basic/project/web/rest/FakturaController.java
+++ project/src/main/java/com/basic/project/web/rest/FakturaController.java
@@ -1,6 +1,12 @@
 package com.basic.project.web.rest;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,10 +19,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.basic.project.domain.Faktura;
 import com.basic.project.service.FakturaService;
 import com.basic.project.web.dto.Converter;
 import com.basic.project.web.dto.FakturaDTO;
 import com.basic.project.web.dto.NarudzbenicaReceiverDTO;
+
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @RestController
 @RequestMapping("/api/faktura")
@@ -37,7 +50,34 @@ public class FakturaController {
 	public ResponseEntity<List<FakturaDTO>> getKif(@RequestParam("datumOd") String datumOd, 
 			@RequestParam("datumDo") String datumDo) {
 		
-		return new ResponseEntity<List<FakturaDTO>>(Converter.convertFakturasToFakturaDTOs(fakturaService.getKif(java.sql.Date.valueOf(datumOd), java.sql.Date.valueOf(datumDo))), HttpStatus.OK);
+		try {
+			
+			List<Faktura> listaFaktura = new ArrayList<Faktura>(fakturaService.getKif(java.sql.Date.valueOf(datumOd), java.sql.Date.valueOf(datumDo)));
+			
+			System.out.println(fakturaService.getAll().size());
+			
+			JRBeanCollectionDataSource itemsJRBean = new JRBeanCollectionDataSource(listaFaktura);
+
+			Map<String, Object> parameters = new HashMap<String, Object>();
+			parameters.put("ItemDataSource", itemsJRBean);
+			
+			JasperPrint jasperPrint = JasperFillManager.fillReport("faktura.jasper", parameters, new JREmptyDataSource());
+		    File file = new File("izvestajKIF.pdf");
+		    OutputStream outputStream = new FileOutputStream(file);
+		    JasperExportManager.exportReportToPdfStream(jasperPrint, outputStream);
+		    
+		    outputStream.close();
+		    
+		    return new ResponseEntity<List<FakturaDTO>>(Converter.convertFakturasToFakturaDTOs(fakturaService.getKif(java.sql.Date.valueOf(datumOd), java.sql.Date.valueOf(datumDo))), HttpStatus.OK);
+
+			
+		} catch(Exception e)
+		{
+			e.printStackTrace();
+		    return new ResponseEntity<List<FakturaDTO>>(Converter.convertFakturasToFakturaDTOs(fakturaService.getKif(java.sql.Date.valueOf(datumOd), java.sql.Date.valueOf(datumDo))), HttpStatus.OK);
+
+		}
+		
 	}
 	
 	// GET ONE
